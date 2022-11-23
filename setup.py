fadd26c1eb7e8b6b650070d48b68663e15cef7a7
Doddi Avinash <avinashdoddi2001@gmail.com>
2022-11-23 19:10:07 +0530

    fastcp v.1.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name = "fastcp",
-    version = "1.0.2",
+    version = "1.0.3",
     author = "Avinash Doddi",
     author_email = "avinashdoddi2001@gmail.com",
     description = "A Python Package for Competitive Programming",
